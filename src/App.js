a1e5017a757d2f4ba59b37013565877271b352c4
svyatoslav lebeckiy <svyatoslav@salo.team>
2022-03-27 15:11:21 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-import botch from './botch.jpg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h4>my clinks</h4>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <div className="App-page">
-        <h5 className="App-h">instagram</h5>
-        <a href="https://www.instagram.com/swietique/">@swietique</a>
-        <a href="https://www.instagram.com/hello.pstrong/">@hello.pstrong</a>
-        <a href="https://www.instagram.com/swetstock/">@swetstock</a>
-
-        <h5 className="App-h">spotify</h5>
-        <h6 className="App-h">liked</h6>
-        <div>
-          <iframe src="https://open.spotify.com/embed/playlist/1Y15VMpFyET2Wc5adzziSp" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-          <iframe src="https://open.spotify.com/embed/playlist/7CsJ24tbS4xDfe8HIY29F9" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-          <iframe src="https://open.spotify.com/embed/playlist/24n6yxbQnFqAZYKEPbWVII" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-          <iframe src="https://open.spotify.com/embed/playlist/56ZLeZsiV6FR1nBh4VXtsg" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        </div>
-        <h6 className="App-h">party</h6>
-        <iframe src="https://open.spotify.com/embed/playlist/29KYholsef5MtOqr3NE41d" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-      </div>
-      <div className="App-page">
-        <img src={botch} className="App-me" alt="Me" />
-      </div>
     </div>
   );
 }
